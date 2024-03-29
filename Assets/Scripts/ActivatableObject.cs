1f2237b27207994362787f82db067c0a3d249ed9
Filomilo <fborowiec@wp.pl>
2024-03-29 12:40:36 +0100

    added openable chests

---
+++ Assets/Scripts/ActivatableObject.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public abstract class ActivatableObject : MonoBehaviour
+{
+
+    [SerializeField] private AudioClip onAcvtivationSound;
+
+    private void OnCollisionEnter2D(Collision2D collsion)
+    {
+        Debug.Log("Collsion: "+ collsion.collider.tag);
+        if (collsion.collider.CompareTag("Player"))
+        {
+            if(onAcvtivationSound!=null)
+                SoundManager.playSound(onAcvtivationSound);
+            onActivate(collsion.collider.gameObject);
+        }
+    }
+
+    protected abstract void onActivate(GameObject player);
+
+}
---
+++ Assets/Scripts/LootChest.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootChest : ActivatableObject
+{
+    [SerializeField] private GameObject[] loot;
+    private bool isOpened = false;
+    protected override void onActivate(GameObject player)
+    {
+        if (!isOpened)
+        {
+            isOpened=true;
+            Debug.Log("chest open");
+            dropLoot();
+        }
+    }
+
+    void pushItem(GameObject obj)
+    {
+        GameObject loot= Instantiate(
+            obj,
+            this.transform.position,
+            new Quaternion(0, 0, 0,0)
+        );
+        float roatation = Random.Range(0,2*Mathf.PI);
+        Vector2 push = new Vector2(Mathf.Sin(roatation), Mathf.Cos(roatation));
+        loot.GetComponent<Rigidbody2D>().AddForce(push * 3,ForceMode2D.Impulse);
+        
+    }
+
+    private void dropLoot()
+    {
+        foreach (GameObject var in loot)
+        {
+            pushItem(var);
+        }
+    }
+}
