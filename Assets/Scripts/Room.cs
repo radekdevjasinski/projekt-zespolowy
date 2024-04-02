d96164571b4eccc4910c15ad70cfeab4f11c4a2a
Jakub Kotwica <01.jakub.kotwica@gmail.com>
2024-04-02 19:08:54 +0200

    RoomScript
    funkcja otwierająca drzwi

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Room : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] GameObject leftDoor;
+    [SerializeField] GameObject rightDoor;
+    [SerializeField] GameObject topDoor;
+    [SerializeField] GameObject bottomDoor;
 
-    // Update is called once per frame
-    void Update()
+    public Vector2Int RoomIndex { get; set; }
+
+    public void OpenDoor(Vector2Int direction)
     {
-        
+        if (direction == Vector2Int.up)
+        {
+            topDoor.SetActive(true);
+        }
+
+        if (direction == Vector2Int.down)
+        {
+            bottomDoor.SetActive(true);
+        }
+
+        if (direction == Vector2Int.left)
+        {
+            leftDoor.SetActive(true);
+        }
+
+        if(direction == Vector2Int.right)
+        {
+            rightDoor.SetActive(true);
+        }
     }
 }
