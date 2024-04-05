2acd05e0703be1203008613ec8998dc18a8f692d
radek.dev <radek.dev.jasinski@gmail.com>
2024-04-05 19:50:20 +0200

    LastMerge

--- Assets/Scripts/Enemy/TestMeleeEnemy.cs
+++ Assets/Scripts/Enemy/TestMeleeEnemy.cs
@@ -10,7 +10,7 @@ public class TestMeleeEnemy : EnemyBase
     // Start is called before the first frame update
     void Start()
     {
-        speed = 3f;
+        //speed = 3f;
         timer = nextAttackTimer;
     }
 
