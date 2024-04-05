da84948e943e82ff9227eede143b2356521f2fa2
radek.dev <radek.dev.jasinski@gmail.com>
2024-04-05 15:39:55 +0200

    PlayerControlsToMain

--- Assets/Scripts/TeleportToNextRoom.cs
+++ Assets/Scripts/TeleportToNextRoom.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class TeleportToNextRoom : MonoBehaviour
 {
-    public DungeonGenerator dungeonGenerator;
-    public DungeonRoom activePlayerRoom;
+    private DungeonGenerator dungeonGenerator;
+    private DungeonRoom activePlayerRoom;
+    private void Start()
+    {
+        dungeonGenerator = GameObject.Find("Dungeon").GetComponent<DungeonGenerator>();
+    }
     public void Teleport(Vector2 triggerPos)
     {
         if (activePlayerRoom == null)
