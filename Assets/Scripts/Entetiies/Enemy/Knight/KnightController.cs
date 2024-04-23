81d78d5e4a1e8e1b5566760cc7de8618bbaa1c27
radek.dev <radek.dev.jasinski@gmail.com>
2024-04-23 22:26:50 +0200

    playable demo

--- Assets/Scripts/Entetiies/Enemy/Knight/KnightController.cs
+++ Assets/Scripts/Entetiies/Enemy/Knight/KnightController.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.U2D.Aseprite;
 using UnityEngine;
 
 public class KnightController : EnemyBase
--- Assets/Scripts/LvlDesignElements/Chest/Chest.cs
+++ Assets/Scripts/LvlDesignElements/Chest/Chest.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.Progress;
-
 public class Chest : MonoBehaviour
 {
     public GameObject itemPrefab;
