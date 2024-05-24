fb2b78b583345d66800224ac1cd4587ec9079873
Jakub Kotwica <01.jakub.kotwica@gmail.com>
2024-05-24 02:01:41 +0200

    Confiner

--- Assets/MenuControl.cs
+++ Assets/MenuControl.cs
@@ -1,18 +1,14 @@
 using Cinemachine;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UIElements;
 
 public class MenuControl : MonoBehaviour
 {
     public CinemachineVirtualCamera CinemachineCamera;
 
-
     private void Start()
     {
-        
+        ChangeConfiner("MenuRoom");
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -29,24 +25,48 @@ public class MenuControl : MonoBehaviour
         else if (collision.CompareTag("Options"))
         {
             TeleportTo(44.3f);
+            ChangeConfiner("OptionsRoom");
         }
         else if (collision.CompareTag("Stats"))
         {
             TeleportTo(-44.3f);
+            ChangeConfiner("StatsRoom");
         }
         else if (collision.CompareTag("OMenu"))
         {
             TeleportTo(5.5f);
+            ChangeConfiner("MenuRoom");
         }
         else if (collision.CompareTag("SMenu"))
         {
             TeleportTo(-7.15f);
+            ChangeConfiner("MenuRoom");
         }
     }
+
     private void TeleportTo(float xPosition)
     {
         transform.position = new Vector2(xPosition, 0.56f);
     }
+
+    private void ChangeConfiner(string roomName)
+    {
+        GameObject room = GameObject.Find(roomName);
+        if (room != null)
+        {
+            PolygonCollider2D roomCollider = room.GetComponentInChildren<PolygonCollider2D>();
+            if (roomCollider != null)
+            {
+                CinemachineCamera.GetComponent<CinemachineConfiner2D>().m_BoundingShape2D = roomCollider;
+            }
+            else
+            {
+                Debug.LogError("PolygonCollider2D not found in room: " + roomName);
+            }
+        }
+        else
+        {
+            Debug.LogError("Room not found: " + roomName);
+        }
+    }
 }
-
-
