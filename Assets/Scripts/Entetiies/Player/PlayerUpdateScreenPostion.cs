b6f323ccda394f3d29f96a3eac885489332e66e3
Filomilo <fborowiec@wp.pl>
2024-05-31 11:46:12 +0200

    commented test code

--- Assets/Scripts/Entetiies/Player/PlayerUpdateScreenPostion.cs
+++ Assets/Scripts/Entetiies/Player/PlayerUpdateScreenPostion.cs
@@ -8,12 +8,11 @@ public class PlayerUpdateScreenPostion : MonoBehaviour
     void LateUpdate()
     {
    
-        Debug.Log("Cmear size: "+ new Vector2(Camera.main.pixelWidth, Camera.main.pixelHeight));
-        Debug.Log("Camera.main.WorldToScreenPoint(transform.position): "+ Camera.main.WorldToScreenPoint(transform.position));
-        Vector4 toUPdate = Camera.main.WorldToScreenPoint(transform.position)/ new Vector2(Camera.main.pixelWidth, Camera.main.pixelHeight);
-        Debug.Log("player pos: "+ toUPdate);
+        //Debug.Log("Cmear size: "+ new Vector2(Camera.main.pixelWidth, Camera.main.pixelHeight));
+        //Debug.Log("Camera.main.WorldToScreenPoint(transform.position): "+ Camera.main.WorldToScreenPoint(transform.position));
+        //Debug.Log("player pos: "+ toUPdate);
         //Debug.Log("player pos global: " + Camera.main.WorldToScreenPoint(transform.position) );
     
-        Shader.SetGlobalVector("_PlayerScreenPosition", toUPdate);
+        Shader.SetGlobalVector("_PlayerScreenPosition", Camera.main.WorldToScreenPoint(transform.position) / new Vector2(Camera.main.pixelWidth, Camera.main.pixelHeight));
     }
 }
