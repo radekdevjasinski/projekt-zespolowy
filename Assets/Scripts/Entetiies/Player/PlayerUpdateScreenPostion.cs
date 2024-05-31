7d4c71222a447f0d677d74d4db0bd96416762526
Filomilo <fborowiec@wp.pl>
2024-05-31 11:40:16 +0200

    fixed player position issuse

--- Assets/Scripts/Entetiies/Player/PlayerUpdateScreenPostion.cs
+++ Assets/Scripts/Entetiies/Player/PlayerUpdateScreenPostion.cs
@@ -2,12 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class PlayerUpdateScreenPostion : MonoBehaviour
 {
     void LateUpdate()
     {
-        //Debug.Log("Cmear size: "+ new Vector2(Screen.height, Screen.width));
-        //Debug.Log("player pos: "+ Camera.main.WorldToScreenPoint(transform.position) / new Vector2(Screen.height, Screen.width));
-        Shader.SetGlobalVector("_PlayerScreenPosition",Camera.main.WorldToScreenPoint(transform.position)/new Vector2(Screen.height,Screen.width));
+   
+        Debug.Log("Cmear size: "+ new Vector2(Camera.main.pixelWidth, Camera.main.pixelHeight));
+        Debug.Log("Camera.main.WorldToScreenPoint(transform.position): "+ Camera.main.WorldToScreenPoint(transform.position));
+        Vector4 toUPdate = Camera.main.WorldToScreenPoint(transform.position)/ new Vector2(Camera.main.pixelWidth, Camera.main.pixelHeight);
+        Debug.Log("player pos: "+ toUPdate);
+        //Debug.Log("player pos global: " + Camera.main.WorldToScreenPoint(transform.position) );
+    
+        Shader.SetGlobalVector("_PlayerScreenPosition", toUPdate);
     }
 }
