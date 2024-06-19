12acaaf9bc8685c6289d685621853dbf418035bf
Wiktoria Wójcik <116677152+Kulencjusz@users.noreply.github.com>
2024-06-19 09:03:35 +0200

    updt
    + wyciszenie

--- Assets/Scripts/Narrator/NarratorDialogControler.cs
+++ Assets/Scripts/Narrator/NarratorDialogControler.cs
@@ -9,6 +9,7 @@ public class NarratorDialogControler : MonoBehaviour
     [SerializeField] private float characterSpeed = 0.04f;
     [SerializeField] private float pause = 2f;
     [SerializeField] private GameObject mumblePack;
+    [SerializeField] private float fadeDuration = 1.0f;
     private RandomLoopSoundControls musicLoopSoundControls;
     private IEnumerator coroutine;
     public bool isTalking;
@@ -54,13 +55,44 @@ public class NarratorDialogControler : MonoBehaviour
     private IEnumerator applyText(string txt)
     {
         dialogueText.text = "";
+        int endLength = (txt.Length / 4) * 2;
+
         for (int i = 0; i < txt.Length; i++)
         {
             dialogueText.text += txt[i];
+            if (i == endLength)
+            {
+                foreach (var source in musicLoopSoundControls.GetComponentsInChildren<AudioSource>())
+                {
+                    StartCoroutine(FadeOutSound());
+                }
+            }
             yield return new WaitForSeconds(characterSpeed);
         }
         yield return new WaitForSeconds(pause);
         musicLoopSoundControls.stopSound();
         isTalking = false;
+    }
+
+    private IEnumerator FadeOutSound()
+    {
+        float currentTime = 0;
+
+        AudioSource[] sources = musicLoopSoundControls.GetComponentsInChildren<AudioSource>();
+
+        while (currentTime < fadeDuration)
+        {
+            currentTime += Time.deltaTime;
+            foreach (var source in sources)
+            {
+                source.volume -= Time.deltaTime / fadeDuration;
+            }
+            yield return null;
+        }
+
+        foreach (var source in sources)
+        {
+            source.volume = 0.1f;
+        }
     }
 }
