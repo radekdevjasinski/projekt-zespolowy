ef1150b7b2ef1a82ae527d11296e1f1fe9dc75ad
Jakub Kotwica <01.jakub.kotwica@gmail.com>
2024-08-20 18:15:04 +0200

    fixed barrel

--- Assets/Scripts/LvlDesignElements/Barrel.cs
+++ Assets/Scripts/LvlDesignElements/Barrel.cs
@@ -1,14 +1,18 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Barrel : MonoBehaviour
 {
     private bool hasStarted = false;
-    public GameObject explosionPrefab;
+    [SerializeField] private GameObject blastPrefab;
+    [SerializeField] private Material explosionMaterial;
+    [SerializeField] private float explosionSpeed = 0.5f;
+    [SerializeField] private float blastRadius = 0.5f;
+    [SerializeField] private float damageModifier = 2.0f;  
+    [SerializeField] private LayerMask damageLayerMask;
+    [SerializeField] private float knockbackPower = 3.0f;
     private DestroyableObject destroyableObject;
     private NavigationBake navigationBake;
-
 
     private void Awake()
     {
@@ -18,7 +22,7 @@ public class Barrel : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if( destroyableObject.health <= 0 && hasStarted == false)
+        if (destroyableObject.health <= 0 && hasStarted == false)
         {
             SpriteRenderer spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
             spriteRenderer.color = Color.red;
@@ -31,9 +35,22 @@ public class Barrel : MonoBehaviour
     {
         hasStarted = true;
         yield return new WaitForSeconds(1f);
-        GameObject newExplosion = Instantiate(explosionPrefab, transform.position, Quaternion.identity);
-        Destroy(this.gameObject);
 
+        GameObject blast = Instantiate(blastPrefab);
+        blast.transform.position = this.transform.position;
+
+        Renderer blastRenderer = blast.GetComponent<Renderer>();
+        blastRenderer.material = explosionMaterial;
+
+        var explosionControl = blast.AddComponent<ExplosionControl>();
+        explosionControl.explosionMaterial = explosionMaterial;
+        explosionControl.explosionSpeed = explosionSpeed;
+
+        explosionControl.StartExplosion(0f, 0.5f);
+
+        DealDamage();
+
+        Destroy(this.gameObject);
 
         if (navigationBake != null)
         {
@@ -43,6 +60,21 @@ public class Barrel : MonoBehaviour
         else
         {
             Debug.LogError("NavigationBake instance not found.");
+        }
+    }
+
+    private void DealDamage()
+    {
+        Collider2D[] affectedObjects = Physics2D.OverlapCircleAll(transform.position, blastRadius, damageLayerMask);
+        foreach (Collider2D obj in affectedObjects)
+        {
+            IDealDamage damageable = obj.GetComponent<IDealDamage>();
+            if (damageable != null)
+            {
+                Vector2 direction = obj.transform.position - transform.position;
+                damageable.dealDamageUniversal(damageModifier, direction);
+
+            }
         }
     }
 }
