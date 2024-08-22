012365d44ad73cf70d856be6a2cff70674202596
radek.dev <radek.dev.jasinski@gmail.com>
2024-08-22 20:20:14 +0200

    merging

--- Assets/Scripts/LvlDesignElements/Barrel.cs
+++ Assets/Scripts/LvlDesignElements/Barrel.cs
@@ -12,10 +12,7 @@ public class Barrel : MonoBehaviour
     [SerializeField] private LayerMask damageLayerMask;
     private DestroyableObject destroyableObject;
     private NavigationBake navigationBake;
-<<<<<<< HEAD
-=======
 
->>>>>>> parent of 9b7bd51 (Revert "Merge remote-tracking branch 'origin/Destroyable-Objects'")
 
     private void Awake()
     {
@@ -55,10 +52,7 @@ public class Barrel : MonoBehaviour
 
         Destroy(this.gameObject);
 
-<<<<<<< HEAD
-=======
 
->>>>>>> parent of 9b7bd51 (Revert "Merge remote-tracking branch 'origin/Destroyable-Objects'")
         if (navigationBake != null)
         {
             Debug.LogError("bake");
@@ -68,7 +62,6 @@ public class Barrel : MonoBehaviour
         {
             Debug.LogError("NavigationBake instance not found.");
         }
-<<<<<<< HEAD
     }
 
     private void DealDamage()
@@ -84,7 +77,5 @@ public class Barrel : MonoBehaviour
 
             }
         }
-=======
->>>>>>> parent of 9b7bd51 (Revert "Merge remote-tracking branch 'origin/Destroyable-Objects'")
     }
 }
